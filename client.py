3b613dc5607a4fefe801444a578fb11f9203de2d
A. Hanif Nursyabana <ahanifnursyabana08@gmail.com>
2025-12-28 22:32:17 +0700

    Tambh Dokumentasi clinet.py

--- client.py
+++ client.py
@@ -9,9 +9,11 @@ BASE_DIR = "File"
 FILENAME = "gambar.png"
 
 try:
+    # Informasi Awal
     print("=== CLIENT FILE TRANSFER ===")
     print(f"Waktu mulai : {datetime.now()}")
 
+    # Setup Socket
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect((SERVER_IP, PORT))
     print("Berhasil terhubung ke server")
@@ -21,6 +23,7 @@ try:
     while b"\n" not in buffer:
         buffer += client.recv(1)
 
+    # Parsing Metadata
     metadata = buffer.decode().strip()
     filename, file_size = metadata.split("|")
     file_size = int(file_size)
@@ -32,10 +35,9 @@ try:
     # Tentukan folder tujuan
     if not os.path.exists(BASE_DIR):
         os.makedirs(BASE_DIR)
-
-    # Path lengkap: File/gambar_terkirim.png
     save_path = os.path.join(BASE_DIR, new_filename)
 
+    # Terima File
     received_size = 0
     with open(save_path, "wb") as f:
         while received_size < file_size:
@@ -45,21 +47,25 @@ try:
             f.write(data)
             received_size += len(data)
 
+            # Progress Bar
             percent = (received_size / file_size) * 100
             print(f"\rProgress terima: {percent:.2f}%", end="")
-
     print("\nTransfer selesai.")
 
+    # Validasi File
     if received_size == file_size:
         print("Validasi: File diterima lengkap.")
     else:
         print("Validasi: File TIDAK lengkap.")
 
+    # Informasi Akhir
     print(f"Disimpan sebagai: {save_path}")
     print(f"Waktu selesai : {datetime.now()}")
 
+    # Tutup Koneksi
     client.close()
 
+# Tangkap Error
 except ConnectionRefusedError:
     print("Gagal terhubung ke server. Pastikan server sudah berjalan.")
 except socket.error as e:
